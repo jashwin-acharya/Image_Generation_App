16810d25370f4360d3602da9f24e515c9bf5dee4
cloutburst <jashwinacharyapersonal@gmail.com>
2023-01-20 16:59:07 -0600

    Installed necessary dependancies

---
+++ client/src/pages/CreateHost.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateHost = () => {
+  return <div>CreateHost</div>;
+};
+
+export default CreateHost;
---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+import Home from "./Home";
+import CreateHost from "./CreateHost";
+
+export { Home, CreateHost };
